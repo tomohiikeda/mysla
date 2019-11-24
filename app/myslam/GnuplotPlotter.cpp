9cdb9778077220775c0723ae855364b06528e7ef
tomohiikeda <tomohi.ikeda@gmail.com>
2019-11-25 01:51:56 +0900

    一時ファイルを/tmpに変更

--- app/myslam/GnuplotPlotter.cpp
+++ app/myslam/GnuplotPlotter.cpp
@@ -22,13 +22,14 @@ void GnuplotPlotter::plot(PointCloud pc)
 {
     if(fd == NULL) return;
     
-    std::ofstream ofs("plot.dat");
+    const char *plotfile = "/tmp/plot.dat";
+    std::ofstream ofs(plotfile);
     for(int i=0; i<pc.size(); i++){
         ofs << pc.at(i).x << " " << pc.at(i).y << std::endl;
     }
     ofs.close();
 
-    fprintf(fd, "plot \"plot.dat\"\n");
+    fprintf(fd, "plot \"%s\"\n", plotfile);
     fflush(fd);
     return;
 }
